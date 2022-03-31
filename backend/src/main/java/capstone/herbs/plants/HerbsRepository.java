28cf17954fddb2c8840fbbdeef748945f8623796
Margret <md601@arcor.de>
2022-03-31 14:53:25 +0200

    Import class collection

--- backend/src/main/java/capstone/herbs/plants/HerbsRepository.java
+++ backend/src/main/java/capstone/herbs/plants/HerbsRepository.java
@@ -3,6 +3,7 @@ package capstone.herbs.plants;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
