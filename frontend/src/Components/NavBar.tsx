39fc6fcf4dfcee9b4cd628f8a6397d41b7176a88
Margret <md601@arcor.de>
2022-04-05 11:04:43 +0200

    navbar

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -2,7 +2,6 @@ import {Link, useNavigate} from "react-router-dom";
 import '../css/NavBar.css';
 
 export default function NavBar(){
-    const nav = useNavigate()
 
     return(
         <div className={'navBar'}>
