45a03ee093e002eb65a4c7eb06efcc5fee46caac
Margret <md601@arcor.de>
2022-04-16 17:03:12 +0200

    IntegrationTest

---
+++ backend/src/main/java/capstone/herbs/HerbsApplication.java
@@ -0,0 +1,13 @@
+package capstone.herbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerbsApplication.class, args);
+    }
+
+}
--- backend/src/test/java/capstone/herbs/UserTest.java
+++ backend/src/test/java/capstone/herbs/UserTest.java
@@ -18,28 +18,13 @@ public class UserTest {
     @Test
     void integrationTest() {
         ResponseEntity<LoginData> createUserResponse = restTemplate.postForEntity("/api/users", new LoginData("Testuser", "123456a", "email@email.de"), LoginData.class);
-        assertThat(createUserResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        ResponseEntity<String> loginResponse = restTemplate.postForEntity("/api/users", new LoginData("Testuser", "123456a", "email@email.de"), String.class);
+        assertThat(createUserResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        ResponseEntity<String> loginResponse = restTemplate.postForEntity("/api/login", new LoginData("Testuser", "123456a", "email@email.de"), String.class);
         assertThat(loginResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(loginResponse.getBody()).isNotBlank();
-
-        ResponseEntity<Void> createUser = restTemplate.exchange(
-                "/api/users",
-                HttpMethod.POST,
-                new HttpEntity<>(new UserNew("Testuser"), createHeaders(loginResponse.getBody())),
-                Void.class
-        );
-        assertThat(createUser.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-
-
     }
-    private HttpHeaders createHeaders(String token) {
-        String authHeader = "Bearer " + token;
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", authHeader);
 
-        return headers;
-    }
+
 }
 
 
