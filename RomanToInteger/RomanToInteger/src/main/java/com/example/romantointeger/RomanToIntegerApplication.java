9e18a347d0a6a39ca3627e9243f96a72e36bb7b1
unknown <md601@arcor.de>
2022-05-31 09:02:55 +0200

    Heroku aktualisieren

---
+++ RomanToInteger/RomanToInteger/src/main/java/com/example/romantointeger/RomanToIntegerApplication.java
@@ -0,0 +1,13 @@
+package com.example.romantointeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanToIntegerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomanToIntegerApplication.class, args);
+    }
+
+}
---
+++ RomanToInteger/RomanToInteger/src/test/java/com/example/romantointeger/RomanToIntegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.romantointeger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanToIntegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
